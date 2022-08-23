ac2937b73d1ef82f7a7e26fed5a64c8ddd817410
Arek Sahal <areksahal@gmail.com>
2022-08-23 21:53:38 +0200

    init again

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import { round } from 'mathjs'
 
 export default function Home() {
+  const [value, setValue] = useState(1200);
+  const [intrest, setIntrest] = useState(0.03)
+
+  const handleChange = event => {
+    setValue(12*event.target.value);
+
+    console.log('value was:', value);
+  };
+  const handleChangeIntrest = event => {
+    setIntrest(event.target.value / 100);
+
+    console.log('value was:', intrest);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +26,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.wrapper}>
+        <div className={styles.amountWrapper}>
+          <label className={styles.amountLabel}> Månadsavgift</label>
+      <input className={styles.amountInput} type="number" onChange={handleChange} placeholder={value/12}></input>
+      </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <div className={styles.intrestWrapper}>
+        <label className={styles.intrestLabel}>
+          Ränta (%)
+        </label>
+        <div className={styles.uselessDiv}>
+      <input className={styles.intrestInput} step={0.1} type="number" onChange={handleChangeIntrest} placeholder={intrest*100}></input>
+      </div>
+      </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <label className={styles.output}> {round(value/intrest).toLocaleString().replaceAll(","," ")} kr</label>
+      <label className={styles.equation}> {round(value/intrest)} x {intrest*100}% = {value/12} x 12</label>
+      <label className={styles.equationExp}> Kapital x ränta = Årsavgift</label>
+      </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
